3498a22ec6d912d4eeae7741706f53143441979b
PhuongPhamDuc2108 <pducphuonghd32108@gmial.com>
2025-03-29 15:36:54 +0700

    add page home

--- HP9/urls.py
+++ HP9/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('introduction/', include('introduction.urls')),
+    #path('introduction/', include('introduction.urls')),
+    path('',include('introduction.urls')),
 ]
